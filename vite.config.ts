c008514038c79c0f0a7ccecd8c7070a1dfd73f30
Chandu Vasa <chandu.vasa@mavenberg.com>
2025-12-14 11:33:29 +0530

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-   base: '/react_coding_assignment/',
+   base: '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
